f9444c245ffb424df0db08b156d53eac39025e87
rafiqwe <rmlmanik502@gmail.com>
2025-11-23 21:48:03 +0600

    Created about component

---
+++ app/components/about/About.tsx
@@ -0,0 +1,11 @@
+import TopAbout from "./TopAbout";
+
+const About = () => {
+  return (
+    <section className="w-full absolute left-0 h-full bg-black">
+      <TopAbout />
+    </section>
+  );
+};
+
+export default About;

ee0a45584a82846ca1c7812d5a199e58ee61c7cd
rafiqwe <rmlmanik502@gmail.com>
2025-11-26 00:09:01 +0600

    add some more section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
-import { LoadingPage } from "./components/LoadingPage";
+import About from "./components/about/About";
 import Hero from "./components/hero/Hero";
-import Cursor from "./components/ui/Cursor";
+import Projects from "./components/projects/Projects";
+import Skill from "./components/Skills/Skill";
+import WaveDivider from "./components/ui/WaveDivider";
+import WaveSeparator from "./components/ui/WaveSeparator";
 
 export default function Home() {
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <About/>
+      <Skill/>
+      <Projects/>
     </div>
-  )
+  );
 }

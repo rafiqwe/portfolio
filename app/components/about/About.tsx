cb251da701c32d7b9b8a9ba65ac65a2fdca3f5d7
rafiqwe <rmlmanik502@gmail.com>
2025-11-28 21:47:06 +0600

    Updated about page

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -1,9 +1,15 @@
+import BottomAbout from "./BottomAbout";
 import TopAbout from "./TopAbout";
+import WaveDivider from "../ui/WaveDivider";
 
 const About = () => {
   return (
-    <section className="w-full absolute left-0 h-full bg-black">
+    <section className="w-full px-6 mt-10 relative overflow-x-clip left-0 h-full bg-black">
+      <div className="absolute -top-20  sm:-top-20  md:-top-30 w-full left-0 z-10">
+        <WaveDivider color="black" />
+      </div>
       <TopAbout />
+      <BottomAbout />
     </section>
   );
 };

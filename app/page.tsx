fc55c2a40d51fabce30016441c97ede4ec34b682
rafiqwe <rmlmanik502@gmail.com>
2025-12-07 22:38:34 +0600

    Add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/footer";
 import Hero from "./components/hero/Hero";
 import Projects from "./components/projects/Projects";
 import Skill from "./components/Skills/Skill";
-import WaveDivider from "./components/ui/WaveDivider";
-import WaveSeparator from "./components/ui/WaveSeparator";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <About/>
-      <Skill/>
-      <Projects/>
+      <About />
+      <Skill />
+      <Projects />
+      <Contact />
+      <div className="h-screen bg-amber-300 "></div>
+      <Footer />
     </div>
   );
 }

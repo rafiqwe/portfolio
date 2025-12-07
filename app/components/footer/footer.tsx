731be8a7d913e8fe88666c1c23b86f21ca45906b
rafiqwe <rmlmanik502@gmail.com>
2025-12-07 23:34:11 +0600

    Updated: footer component

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,31 +1,107 @@
-import React from "react";
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Link from "next/link";
 
 const Footer = () => {
+  const socialMedia = [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/muhammad-rabbi-dev",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/muhammadrabbi.dev/",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/rafiqwe",
+    },
+  ];
+
+  const menu = [
+    {
+      name: "Home",
+      url: "#",
+    },
+    {
+      name: "About",
+      url: "#about",
+    },
+    {
+      name: "Skills",
+      url: "#skills",
+    },
+    {
+      name: "Projects",
+      url: "#projects",
+    },
+    {
+      name: "Contact",
+      url: "#contact",
+    },
+  ];
+
+  const handleMouseEnter = () => {
+    gsap.to(".cursor", {
+      scale: 5,
+      duration: 0.3,
+      textContent: "Click",
+      color: "black",
+      fontSize: "4px",
+      ease: "power2.out",
+    });
+  };
+
+  const handleMouseLeave = () => {
+    gsap.to(".cursor", {
+      scale: 1,
+      duration: 0.3,
+      textContent: "",
+      color: "white",
+      textAlign: "center",
+      fontSize: "20px",
+      ease: "power2.out",
+    });
+  };
+
+  useGSAP(() => {
+    handleMouseEnter();
+    handleMouseLeave();
+  });
+
   return (
     <footer className="bg-black text-white p-5 h-screen md:p-10">
       <div className="max-w-7xl mx-auto">
-        <div className="flex flex-col md:flex-row justify-between">
-          <div>
-            <h1 className="font-corporatus text-5xl">Menu</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Skills</li>
-              <li>Projects</li>
-              <li>Contact</li>
+        <div className="flex flex-col md:flex-row pt-20 max-w-5xl gap-10 justify-between">
+          <div className="w-full">
+            <div className="border-b-3 border-white/20 pb-2">
+              <h1 className="font-corporatus text-2xl ">Menu</h1>
+            </div>
+            <ul className="flex flex-col gap-6 mt-5 font-corporatus text-lg">
+              {menu.map((item) => (
+                <li
+                  onMouseEnter={handleMouseEnter}
+                  key={item.name}
+                  onMouseLeave={handleMouseLeave}
+                  className="cursor-pointer w-fit"
+                >
+                  <Link href={item.url}>{item.name}</Link>
+                </li>
+              ))}
             </ul>
           </div>
-          <div>
+          <div className="w-1/2">
             <div>
-              <h1 className="font-corporatus text-5xl">Contact</h1>
-              <p>Feel free to reach out or follow me!</p>
-            </div>
-            <div>
-              <h1>Social Media</h1>
-              <ul>
-                <li>Linkedin</li>
-                <li>Instagram</li>
-                <li>Github</li>
+              <div className="border-b-3 border-white/20 pb-2">
+                <h1 className="font-corporatus text-2xl ">Social Media</h1>
+              </div>
+              <ul className="flex flex-col gap-6 mt-5 font-mono text-lg">
+                {socialMedia.map((item) => (
+                  <li key={item.name}>
+                    <Link href={item.url}>{item.name}</Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>

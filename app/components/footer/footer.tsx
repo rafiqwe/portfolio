8a01361a5067b2618f30c0273ed7b2fc45c459d3
rafiqwe <rmlmanik502@gmail.com>
2025-12-08 23:30:39 +0600

    Updated: footer coponent.Add footer menu & footer social links add update style also

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,21 +1,23 @@
-"use client";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import Link from "next/link";
+import GlowLine from "../ui/GlowLine";
+import FooterMenu from "./FooterMenu";
+import SocialLink from "./SocialLink";
 
 const Footer = () => {
   const socialMedia = [
     {
       name: "Linkedin",
       url: "https://www.linkedin.com/in/muhammad-rabbi-dev",
+      image: "/images/linkedin1.webp",
     },
     {
       name: "Instagram",
       url: "https://www.instagram.com/muhammadrabbi.dev/",
+      image: "/images/instagram.webp",
     },
     {
       name: "Github",
       url: "https://github.com/rafiqwe",
+      image: "/images/github.webp",
     },
   ];
 
@@ -42,36 +44,11 @@ const Footer = () => {
     },
   ];
 
-  const handleMouseEnter = () => {
-    gsap.to(".cursor", {
-      scale: 5,
-      duration: 0.3,
-      textContent: "Click",
-      color: "black",
-      fontSize: "4px",
-      ease: "power2.out",
-    });
-  };
-
-  const handleMouseLeave = () => {
-    gsap.to(".cursor", {
-      scale: 1,
-      duration: 0.3,
-      textContent: "",
-      color: "white",
-      textAlign: "center",
-      fontSize: "20px",
-      ease: "power2.out",
-    });
-  };
-
-  useGSAP(() => {
-    handleMouseEnter();
-    handleMouseLeave();
-  });
-
   return (
-    <footer className="bg-black text-white p-5 h-screen md:p-10">
+    <footer className="bg-black relative text-white p-5 h-[70vh] md:p-10">
+      <div className="absolute top-0 left-0 w-full h-full ">
+        <GlowLine orientation="horizontal" position="0" color="blue" />
+      </div>
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-col md:flex-row pt-20 max-w-5xl gap-10 justify-between">
           <div className="w-full">
@@ -80,14 +57,7 @@ const Footer = () => {
             </div>
             <ul className="flex flex-col gap-6 mt-5 font-corporatus text-lg">
               {menu.map((item) => (
-                <li
-                  onMouseEnter={handleMouseEnter}
-                  key={item.name}
-                  onMouseLeave={handleMouseLeave}
-                  className="cursor-pointer w-fit"
-                >
-                  <Link href={item.url}>{item.name}</Link>
-                </li>
+                <FooterMenu key={item.name} item={item} />
               ))}
             </ul>
           </div>
@@ -98,9 +68,7 @@ const Footer = () => {
               </div>
               <ul className="flex flex-col gap-6 mt-5 font-mono text-lg">
                 {socialMedia.map((item) => (
-                  <li key={item.name}>
-                    <Link href={item.url}>{item.name}</Link>
-                  </li>
+                  <SocialLink key={item.name} item={item} />
                 ))}
               </ul>
             </div>

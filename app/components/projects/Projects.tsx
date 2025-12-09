f6ceb6ba35a2bccc8d3f1e9f05c052c7aa44426b
rafiqwe <rmlmanik502@gmail.com>
2025-12-09 23:35:33 +0600

    Updated: project component

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -1,13 +1,14 @@
 import ProjectsCard from "./ProjectsCard";
 
+
 const Projects = () => {
   return (
-    <div className="w-full h-full text-white bg-black">
-      <div className="flex flex-col items-center justify-center pt-20 mb-10">
-        <h1 className="font-corporatus text-5xl">Projects</h1>
+      <div className="w-full h-full text-white bg-black">
+        <div className="flex flex-col items-center justify-center pt-20 mb-10">
+          <h1 className="font-corporatus text-5xl">Projects</h1>
+        </div>
+        <ProjectsCard />
       </div>
-      <ProjectsCard/>
-    </div>
   );
 };
 

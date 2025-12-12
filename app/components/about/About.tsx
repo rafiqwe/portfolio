5e9dc515bd31bcaddd552d767c2bdc98fa487e6f
rafiqwe <rmlmanik502@gmail.com>
2025-12-12 21:24:51 +0600

    Updated: About component

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -4,7 +4,10 @@ import WaveDivider from "../ui/WaveDivider";
 
 const About = () => {
   return (
-    <section className="w-full px-6 mt-10 relative overflow-x-clip left-0 h-full bg-black">
+    <section
+      id="about"
+      className="w-full px-6 mt-10 relative overflow-x-clip left-0 h-full bg-black"
+    >
       <div className="absolute -top-20  sm:-top-20  md:-top-30 w-full left-0 z-10">
         <WaveDivider color="black" />
       </div>

1f5c33331b6bdd8b07d78c68143c01e38a6c928a
rafiqwe <rmlmanik502@gmail.com>
2025-12-12 21:22:02 +0600

    fixed: footer menu bug

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -24,23 +24,23 @@ const Footer = () => {
   const menu = [
     {
       name: "Home",
-      url: "#",
+      url: "",
     },
     {
       name: "About",
-      url: "#about",
+      url: "about",
     },
     {
       name: "Skills",
-      url: "#skills",
+      url: "skill",
     },
     {
       name: "Projects",
-      url: "#projects",
+      url: "projects",
     },
     {
       name: "Contact",
-      url: "#contact",
+      url: "contact",
     },
   ];
 
@@ -49,13 +49,13 @@ const Footer = () => {
       <div className="absolute top-0 left-0 w-full h-full ">
         <GlowLine orientation="horizontal" position="0" color="blue" />
       </div>
-      <div className="max-w-7xl mx-auto text-center md:text-left">
+      <div className="max-w-7xl relative mx-auto text-center md:text-left">
         <div className="flex flex-col md:flex-row pt-20 max-w-5xl gap-10 justify-between">
           <div className="w-full ">
             <div className="border-b-3 border-white/20 pb-2">
               <h1 className="font-corporatus text-2xl ">Menu</h1>
             </div>
-            <ul className="flex flex-col gap-6 mt-5 w-full md:items-start items-center font-corporatus text-center text-lg">
+            <ul className="flex flex-col gap-6 mt-5 w-full md:items-start items-center font-corporatus text-center cursor-pointer text-lg">
               {menu.map((item) => (
                 <FooterMenu key={item.name} item={item} />
               ))}

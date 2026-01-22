d9d8135ff3851aa38d9d15b49aab911b33cfa083
rafiqwe <rmlmanik502@gmail.com>
2026-01-22 16:54:26 +0600

    deleted

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/footer";
 import Hero from "./components/hero/Hero";
-import MoblieNav from "./components/hero/MobileNav";
 import Projects from "./components/projects/Projects";
 import Skill from "./components/Skills/Skill";
 
